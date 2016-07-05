d2d5ff1e59ef422b0922509cfb8f4d99a58ab285
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-07-04 21:13:56 -0300

    2nd

---
+++ src/controller/PotMethods.java
@@ -0,0 +1,11 @@
+
+package controller;
+
+public interface PotMethods {
+    
+    public void create (Object object){
+    objects.add(object);
+    
+    }
+    
+}
--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,11 +1,9 @@
 
 package model;
 
-public class Aluno {
+public class Aluno extends Pessoa {
     
     private int matricula;
-    private String nome;
-    private int idade;
 
     public int getMatricula() {
         return matricula;
@@ -15,27 +13,6 @@ public class Aluno {
         this.matricula = matricula;
     }
 
-    public String getNome() {
-        return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    @Override
-    public String toString() {
-        return "Aluno{" + "matricula=" + matricula + ", nome=" + nome + ", idade=" + idade + '}';
-    }
-    
-    
-    
 }
